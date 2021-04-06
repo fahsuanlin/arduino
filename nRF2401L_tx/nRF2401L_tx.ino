2ab7a8bea4ed21e94a7c5d1f3a5f2507613a7055
fahsuanlin <fahsuanlin@gmail.com>
2021-04-05 21:11:26 -0400

    040521

---
+++ nRF2401L_tx/nRF2401L_tx.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+int button_pin = 2;
+boolean button_state = 0;
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Tx");
+  
+  pinMode(button_pin, INPUT);
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();          //This sets the module as transmitter
+}
+void loop()
+{
+  button_state = digitalRead(button_pin);
+  if (button_state == HIGH)
+  {
+    Serial.println("TX:HIGH");
+    const char text[] = "Your Button State is HIGH";
+    radio.write(&text, sizeof(text));                  //Sending the message to receiver
+  }
+  else
+  {
+    Serial.println("TX:LOW");
+    const char text[] = "Your Button State is LOW";
+    radio.write(&text, sizeof(text));                  //Sending the message to receiver
+  }
+  radio.write(&button_state, sizeof(button_state));  //Sending the message to receiver
+  delay(1000);
+}

f69a1842e6307ee00a48dfb5fc87ec09d5cb7701
fa-hsuan lin <fahsuanlin@gmail.com>
2021-10-03 10:59:17 -0400

    100321

--- trigger_usb_ttl/trigger_usb_ttl.ino
+++ trigger_usb_ttl/trigger_usb_ttl.ino
@@ -1,10 +1,12 @@
-int led = 13;
+int ttl_port = 3;
+int debounce_time = 200; //debounce time; ms
+
 int led_status = 0;
 
 void setup() {
 
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  pinMode(ttl_port, OUTPUT);
 }
 
 void loop() {
@@ -12,15 +14,15 @@ void loop() {
   if (Serial.available()) {      // If anything comes in Serial (USB),
     //Serial.println("INPUT");
     int incomingByte = Serial.read();
-    if (incomingByte>47) { //"0": off; "1": on; ASCII code
-      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    if (incomingByte>48) { //"0": off; "1": on; ASCII code
+      digitalWrite(ttl_port, HIGH);   // turn the LED on (HIGH is the voltage level)
       led_status = 1;
-      delay(200); //debounce
+      delay(debounce_time); //debounce
     }
     else {
     //Serial.println("NOTHING");
       if(led_status>0){
-        digitalWrite(led, LOW);
+        digitalWrite(ttl_port, LOW);
         led_status = 0;
       }
     }
--- trigger_usb_ttl/trigger_usb_ttl.m
+++ trigger_usb_ttl/trigger_usb_ttl.m
@@ -1,4 +1,10 @@
-device = serialport("/dev/tty.usbmodem143201",9600);
+close all; clear all;
+
+devices=serialportlist;
+
+%device = serialport("/dev/tty.usbmodem143201",9600);
+device = serialport(devices(end),9600);
+
 for i=1:10
     writeline(device,sprintf('%d',mod(i,2)))
     pause(1);

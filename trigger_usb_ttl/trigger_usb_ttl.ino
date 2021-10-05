17b15b85cb757b276ba6bfcd87411a38ab4cd6a0
Fa-Hsuan Lin <fahsuanlin@gmail.com>
2021-10-04 22:20:51 -0400

    100421

--- trigger_usb_ttl/trigger_usb_ttl.ino
+++ trigger_usb_ttl/trigger_usb_ttl.ino
@@ -1,62 +1,103 @@
-int ttl_port = 3;
-int pass_port = 5; 
+char trigger_char = '1'; //trigger for TTL pulse
+char end_char = '9'; //end-of-experiment
 
-int time_ttl; //time instant for input
+int ttl_port = 3; //port for sending TTL pulse
+int pass_port = 5;  //port for forwarding input
 
+unsigned long time_ttl; //time instant for input
+unsigned long time_ttl_buffer[200]; //time instant for input
 int debounce_time_ttl = 500; //debounce time; ms
+unsigned long n_ttl;
 
 int led_status = 0;
 
-int time_input; //time instant for input
+unsigned long time_input; //time instant for input
+int debounce_time_input = 500; //debounce time; ms
+unsigned long n_input;
 
-int debounce_time_input= 500; //debounce time; ms
-
+byte buf[4];
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(57600);
   pinMode(ttl_port, OUTPUT);
   pinMode(pass_port, OUTPUT);
 
   time_input = millis();
-  time_ttl=0;
+  n_input = 0;
+
+  time_ttl = 0;
+  n_ttl = 0;
 }
 
 void loop() {
 
-   if(millis()-time_input>debounce_time_input){
-        digitalWrite(pass_port, LOW);   // turn the LED off
-    }
+  if (millis() - time_input > debounce_time_input) {
+    digitalWrite(pass_port, LOW);   // turn the input LED off
+  }
 
-   if(millis()-time_ttl>debounce_time_ttl){
-        digitalWrite(ttl_port, LOW);   // turn the LED off
-    }
-    
+  if (millis() - time_ttl > debounce_time_ttl) {
+    digitalWrite(ttl_port, LOW);   // turn the TTL LED off
+  }
+
   if (Serial.available()) {      // If anything comes in Serial (USB),
     //Serial.println("INPUT");
     int incomingByte = Serial.read();
 
+
     //receive input?
-    if (incomingByte>0) {
+    if (incomingByte > 0 && incomingByte != end_char) {
       digitalWrite(pass_port, HIGH);   // turn the LED on (HIGH is the voltage level)
-      time_input = millis();     
+      n_input++;
+
+      if (millis() - time_input > debounce_time_input) {
+
+        time_input = millis();
+
+        buf[0] = time_input & 255;
+        buf[1] = (time_input >> 8)  & 255;
+        buf[2] = (time_input >> 16) & 255;
+        buf[3] = (time_input >> 24) & 255;
+
+        Serial.write(buf, sizeof(buf));
+      }
     }
 
     //receive trigger?
-    if (incomingByte>48) { //"0": off; "1": on; ASCII code
-       if(millis()-time_ttl>debounce_time_ttl){
-          digitalWrite(ttl_port, HIGH);   // turn the LED on (HIGH is the voltage level)
-          time_ttl=millis();
-       }
+    if (incomingByte == trigger_char) {  //"1" as the trigger character
+      if (millis() - time_ttl > debounce_time_ttl) {
+        digitalWrite(ttl_port, HIGH);   // turn the LED on (HIGH is the voltage level)
+        time_ttl = millis();
+        time_ttl_buffer[n_ttl] = time_ttl;
+        n_ttl++;
+      }
       led_status = 1;
     }
+    else if (incomingByte == end_char) { //"9" as the end-of-experiment character; send total number of TTL pulses and their timing stamps
+
+      buf[0] = n_ttl & 255;
+      buf[1] = (n_ttl >> 8)  & 255;
+      buf[2] = (n_ttl >> 16) & 255;
+      buf[3] = (n_ttl >> 24) & 255;
+
+      Serial.write(buf, sizeof(buf));
+
+      for (int index = 0; index < n_ttl; index++) {
+        buf[0] = time_ttl_buffer[index] & 255;
+        buf[1] = (time_ttl_buffer[index] >> 8)  & 255;
+        buf[2] = (time_ttl_buffer[index] >> 16) & 255;
+        buf[3] = (time_ttl_buffer[index] >> 24) & 255;
+
+        Serial.write(buf, sizeof(buf));
+      }
+    }
     else {
-    //Serial.println("NOTHING");
-      if(led_status>0){
-         if(millis()-time_ttl>debounce_time_ttl){
+      //Serial.println("NOTHING");
+      if (led_status > 0) {
+        if (millis() - time_ttl > debounce_time_ttl) {
           digitalWrite(ttl_port, LOW);
           led_status = 0;
-         }
+        }
       }
     }
   }
--- trigger_usb_ttl/trigger_usb_ttl.m
+++ trigger_usb_ttl/trigger_usb_ttl.m
@@ -1,12 +1,85 @@
 close all; clear all;
 
+buffer=[];
+tmp1=[];
+tmp2=[];
+tmp3=[];
+tmp4=[];
+
+flag_stop=0;
+
 devices=serialportlist;
-
 %device = serialport("/dev/tty.usbmodem143201",9600);
-device = serialport(devices(end),9600);
+device = serialport(devices(end),57600); %Typically the last device is for the plugged-in trigger box. It may vary though.
+%configureTerminator(device,"LF");
 
-for i=1:11
-    writeline(device,sprintf('%d',mod(i,2)))
+
+%clear USB port buffer
+while(~flag_stop)
+    tmp1=read(device,1,'uint8');
+    if(isempty(tmp1))
+        flag_stop=1;
+    end;
+end;
+
+
+%clear USB port buffer
+for i=1:12
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % send trigger here
+    writeline(device,sprintf('%d',mod(i,2)));
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % read incoming data for time stamps
+    tmp1=read(device,1,'uint8');
+    if(~isempty(tmp1))
+        tmp2=read(device,1,'uint8');
+        tmp3=read(device,1,'uint8');
+        tmp4=read(device,1,'uint8');
+    
+        buffer(i) = bitand(tmp1,255)+bitand(tmp2,255)*256+bitand(tmp3,255)*256*256+bitand(tmp4,255)*256*256*256;
+    end; 
+    
+    fprintf('received by box for input [%d] at %d (ms)\n',i,buffer(i));     
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
     pause(1);
 end;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%end of experiment
+writeline(device,sprintf('%d',9));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% read incoming data for time stamps
+tmp1=read(device,1,'uint8');
+if(~isempty(tmp1))
+    tmp2=read(device,1,'uint8');
+    tmp3=read(device,1,'uint8');
+    tmp4=read(device,1,'uint8');
+    
+    n_ttl = bitand(tmp1,255)+bitand(tmp2,255)*256+bitand(tmp3,255)*256*256+bitand(tmp4,255)*256*256*256;
+else
+    n_ttl=0;
+end;
+
+fprintf('[%d] trigger sent!\n',n_ttl);
+
+for index=1:n_ttl
+    tmp1=read(device,1,'uint8');
+    if(~isempty(tmp1))
+        tmp2=read(device,1,'uint8');
+        tmp3=read(device,1,'uint8');
+        tmp4=read(device,1,'uint8');
+        
+        ttl_time(index) = bitand(tmp1,255)+bitand(tmp2,255)*256+bitand(tmp3,255)*256*256+bitand(tmp4,255)*256*256*256;
+    end;
+    fprintf('TTL [%d]: %d (ms)\n',index,ttl_time(index));
+end;
+
+
+%close port
 delete(device)
+

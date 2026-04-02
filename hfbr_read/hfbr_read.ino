f331dbb5dca3178a1e1d70facb8ffb61205cbe3b
Fa-Hsuan Lin <fhlin@sri.utoronto.ca>
2026-04-02 14:53:58 -0400

    040226

---
+++ hfbr_read/hfbr_read.ino
@@ -0,0 +1,78 @@
+/*
+   HFBR Fiber Receiver to TTL Output
+   Input: Pin D2 (Active Low from HFBR)
+   Output: Pin D3 (Active High "FLIP!" signal)
+*/
+
+#include <U8g2lib.h>
+#include <U8x8lib.h>
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+
+String led_string;
+char led_string_buffer[50];
+
+const int fiberInputPin = 2;
+const int flipOutputPin = 3; // This pin will provide the "FLIP!" signal
+const int onboardLed = 13;   // Standard Nano LED
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
+
+void setup() {
+
+  u8g2.begin();
+
+  pinMode(fiberInputPin, INPUT);
+  pinMode(flipOutputPin, OUTPUT);
+  pinMode(onboardLed, OUTPUT);
+
+  // Ensure outputs start at 0V
+  digitalWrite(flipOutputPin, LOW);
+  digitalWrite(onboardLed, LOW);
+
+  Serial.begin(9600);
+  Serial.println("System Active: Monitoring Fiber for FLIP signal...");
+}
+
+void loop() {
+  // Check if D2 is pulled LOW by the HFBR receiver
+  if (digitalRead(fiberInputPin) == LOW) {
+
+    // 1. Trigger the physical FLIP! output
+    digitalWrite(flipOutputPin, HIGH);
+
+    // 2. Trigger the onboard diagnostic LED
+    digitalWrite(onboardLed, HIGH);
+
+    // 3. Log to Serial Monitor
+    Serial.println("FLIP! Pulse Generated on D3");
+
+    led_string = "ON";
+    led_string.toCharArray(led_string_buffer, 50);
+    u8g2.clearBuffer();          // clear the internal memory
+    u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+    u8g2.drawStr(8, 29, led_string_buffer);
+    u8g2.sendBuffer();         // transfer internal memory to the display
+
+    // 4. Pulse Duration (Stretched to 100ms so you can see/measure it easily)
+    delay(100);
+
+    // 5. Reset both pins to LOW
+    digitalWrite(flipOutputPin, LOW);
+    digitalWrite(onboardLed, LOW);
+
+    led_string = "OFF";
+    led_string.toCharArray(led_string_buffer, 50);
+    u8g2.clearBuffer();          // clear the internal memory
+    u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+    u8g2.drawStr(8, 29, led_string_buffer);
+    u8g2.sendBuffer();         // transfer internal memory to the display
+
+    // 6. Lockout: Wait for the fiber to go dark again before allowing another flip
+    while (digitalRead(fiberInputPin) == LOW) {
+      delay(1);
+    }
+  }
+}
--- trigger_usb_ttl_bidir_micro/trigger_usb_ttl_bidir_micro.ino
+++ trigger_usb_ttl_bidir_micro/trigger_usb_ttl_bidir_micro.ino
@@ -4,8 +4,8 @@
 char trigger_char = '1'; //trigger for TTL pulse
 char end_char = '9'; //end-of-experiment
 
-int ttl_output = 5;  //port for forwarding input; D5
-int ttl_input = 4;  //port for notifying received TTL pulse; D4
+int ttl_output = 5;  //port for forwarding input; 5
+int ttl_input = 4;  //port for notifying received TTL pulse; 4
 int ttl_input_monitor = 19;  //port for notifying received TTL pulse; A1 (Micro)
 int ttl_input_bridge_monitor = 20;  //port for briding received TTL pulse; A2  (Micro)
 char ttl_ipput_response_char = '5';
@@ -13,8 +13,8 @@ char ttl_ipput_response_char = '5';
 //int pass_port_in = 2;  //port for receiving TT: pulse; D2
 //int ttl_port = 3; //port for sending TTL pulse
 
-int ttl_output_debounce_time = 100; //debounce time; ms
-int ttl_input_debounce_time = 1000; //debounce time; ms
+int ttl_output_debounce_time = 200; //debounce time; ms
+int ttl_input_debounce_time = 200; //debounce time; ms
 int ttl_input_duration_time = 10; //trigger output duration; ms
 
 
@@ -78,7 +78,7 @@ void loop() {
     if (incomingTTL > 500) {
       digitalWrite(ttl_input, HIGH);   // turn the LED on (HIGH is the voltage level)
 
-      Keyboard.press(ttl_ipput_response_char); // send Keyboard stroke
+      Keyboard.write(ttl_ipput_response_char); // send Keyboard stroke
 
       if (bridge_flag > 500) { // bridge ttl input to output
         digitalWrite(ttl_output, HIGH);   // turn the LED on (HIGH is the voltage level)
